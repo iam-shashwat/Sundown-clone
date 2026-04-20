2cb25dce808e47244ee6f355d47a838a59aed01d
Shashwat Sharma <shashwat.manasvi@gmail.com>
2026-04-20 15:31:08 +0530

    till moving text

---
+++ script.js
@@ -0,0 +1,10 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true,
+    smartphone: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
+});
